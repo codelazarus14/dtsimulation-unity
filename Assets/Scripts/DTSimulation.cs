4af03c9a8b7632609c8d71f84f3005cf1a0940bf
codelazarus14 <codel1763@gmail.com>
2024-06-11 13:05:46 -0400

    implemented Tidy

--- Assets/Scripts/DTSimulation.cs
+++ Assets/Scripts/DTSimulation.cs
@@ -17,20 +17,31 @@ namespace DTSimulation
         private const int DISKVOL = 472;
         private const int DISKMINVOL = DISKVOL - 30;
         private const int DISKMAXVOL = DISKVOL + 30;
+        private const float FRAC = 0.75f;
+        private const float TC = 7f;
         private const int MARKEDQ = 282; //FRAC*(DISKVOL+2)/(2.0-FRAC)
-
-        private const int VOL = DISKVOL * MARKEDQ;
-        private const int BIGVOL = 4 * VOL;
 
         private const int D = 2;
         private const int DPLUS = D + 1;
+        private const int DPLUSPLUS = D + 2;
+        private const int VOL = DISKVOL + MARKEDQ;
+        private const int BIGVOL = 4 * VOL;
+        private const int MAXVOL = VOL + 30;
+        private const int MINVOL = DPLUSPLUS;
+        private const int NUMROW = VOL / 2;
+        private const int NONZEROS = NUMROW * VOL; // over 8 billion???
 
         /* simple global pointers and counters */
 
-        public Node stack_head = null;
+        private Node stack_head = null;
         private int simplex_number = 0;
         private int pointer_number = 0;
         private int node_number = 0;
+
+        private int[] boundary;
+
+        private int[] nstart, ncol;
+        private double[] nlap;
 
         /* data structures */
 
@@ -39,8 +50,11 @@ namespace DTSimulation
 
         /* measurements etc */
 
+        private int number_measure = 0, max_point;
+        private int[] legal_subsimplex, try_subsimplex, manifold_subsimplex, go_subsimplex;
         private int vol_monitor = 0, num_monitor = 0, b_monitor = 0, growing_vol;
 
+        private bool grow;
 
         //
         // my fields/deviations from above
@@ -78,11 +92,46 @@ namespace DTSimulation
             simplex_point = new Simplex[BIGVOL];
             ReadFile();
 
+            nlap = new double[NONZEROS];
+            ncol = new int[NONZEROS];
+            nstart = new int[VOL];
+            boundary = new int[VOL];
+
+            legal_subsimplex = new int[DPLUS];
+            try_subsimplex = new int[DPLUS];
+            manifold_subsimplex = new int[DPLUS];
+            go_subsimplex = new int[DPLUS];
+
+            /* build lattice */
+
+
+            //grow = LOGIC.NO;
+            //if (grow == LOGIC.YES)
+            //{
+            //    while (growing_vol < VOL)
+            //    {
+            //        //System.out.println(growing_vol);
+            //        trial_change();
+            //        growing_vol += D;
+            //        //PrintConfig();
+            //    }
+            //}
+
+            Tidy();
+
             //...
         }
 
         private void ReadFile()
         {
+            // TODO: store state in some kind of struct that can be serialized/inspected
+            // the temp field, for example, is literally just throwing away some of the Scanner-esque outputs
+            // bc those fields are only edited at runtime, not loaded in
+            // basically this is all based on a fragile text-based structure that has to be 1-1 with the whitespaced config file
+            // when using a structured JSON type thing would save a lot of headaches and look better
+
+            // also TODO: maybe create a textmesh to store these outputs like an in-game console cuz the debug one is not great
+
             // load config
             string[] configLines = config.text.Split(new char[0], StringSplitOptions.RemoveEmptyEntries);
             // using a string stack to imitate use of the Scanner in the java version
@@ -184,6 +233,33 @@ namespace DTSimulation
 
             stack_head = temp;
             stack_count++;
+            return;
+        }
+
+        private void Tidy()
+        {
+            Simplex[] temp = new Simplex[BIGVOL];
+            int add = 0;
+
+            if (pointer_number > max_point) max_point = pointer_number;
+
+            for (int i = 0; i < pointer_number; i++)
+            {
+                if (simplex_point[i] != null)
+                {
+                    temp[add] = simplex_point[i];
+                    temp[add].label = add;
+                    add++;
+                }
+            }
+
+            for (int i = 0; i < add; i++)
+                simplex_point[i] = temp[i];
+
+            pointer_number = add;
+            if (pointer_number != simplex_number)
+                Debug.Log("oops - pointer number is not equal to simplex_number in tidy()");
+
             return;
         }
     }
